6d1e1ef42ff027ed28415675fd99603e8cc9d88e
Caius Brindescu <caius.brindescu@gmail.com>
2011-10-13 23:19:24 +0300

    Added two more panes

---
+++ src/usr/caiusb/timetracker/ProjectPane.java
@@ -0,0 +1,18 @@
+package usr.caiusb.timetracker;
+
+import javax.swing.JPanel;
+
+public class ProjectPane extends ContentPane {
+
+	public ProjectPane(MainWindow owner) {
+		super(owner);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public JPanel getPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
